c5fc075d442bed700a528bfad6e0d254e7f26b8c
Akmadan23 <azadahmadi@mailo.com>
2023-01-02 12:09:21 +0100

    Add `builder_derive` attribute

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,43 @@
 use proc_macro::TokenStream;
 use quote::{ quote, format_ident };
-use syn::{ parse_macro_input, Data, DeriveInput };
 
-#[proc_macro_derive(SerdeBuilder)]
+use syn::{
+    parse_macro_input,
+    Attribute,
+    Data,
+    DeriveInput,
+    Ident,
+};
+
+trait EqStr {
+    fn eq_str(&self, s: &str) -> bool;
+}
+
+impl EqStr for Ident {
+    fn eq_str(&self, s: &str) -> bool {
+        *self == Self::new(s, self.span())
+    }
+}
+
+#[proc_macro_derive(SerdeBuilder, attributes(builder_derive))]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let DeriveInput { ident, data, vis, .. } = parse_macro_input!(input);
+    let DeriveInput { ident, data, vis, attrs, .. } = parse_macro_input!(input);
     let (mut field_names, mut types) = (vec![], vec![]);
+    let mut derives = quote!();
     let ident_builder = format_ident!("{}Builder", ident);
 
     let Data::Struct(s) = data else {
         panic!("SerdeBuilder only works for structs.");
     };
+
+    for Attribute { path, tokens, .. } in attrs {
+        let ident = path.get_ident().expect("Unexpected attribute.");
+
+        if ident.eq_str("builder_derive") {
+            derives = quote! { #[derive #tokens] };
+            break
+        }
+    }
 
     for f in s.fields {
         field_names.push(f.ident.unwrap());
@@ -18,6 +45,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
     }
 
     quote! {
+        #derives
         #vis struct #ident_builder {
             // TODO: if type is already `Option<_>` don't manipulate it
             #(pub #field_names: Option<#types>),*
