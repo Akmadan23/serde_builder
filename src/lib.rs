e516805ef325926610765b2d87ec6f0433b94f7c
Akmadan23 <azadahmadi@mailo.com>
2023-01-04 11:12:17 +0100

    Add `use_builder` attr. for nested builder structs

--- src/lib.rs
+++ src/lib.rs
@@ -21,7 +21,7 @@ impl EqStr for Ident {
     }
 }
 
-#[proc_macro_derive(SerdeBuilder, attributes(builder_derive))]
+#[proc_macro_derive(SerdeBuilder, attributes(builder_derive, use_builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let DeriveInput { ident, data, vis, attrs, .. } = parse_macro_input!(input);
     let (mut field_names, mut types, mut build_instructions) = (vec![], vec![], vec![]);
@@ -41,16 +41,44 @@ pub fn derive(input: TokenStream) -> TokenStream {
         }
     }
 
-    for Field { ident: f_ident, ty, .. } in s.fields {
+    for Field { ident: f_ident, attrs: f_attrs, ty, .. } in s.fields {
         let Type::Path(f_type) = ty else {
             panic!("Type not supported");
         };
+
+        let mut is_builder = false;
+        let mut f_type_builder = f_type.clone();
+
+        if let Some(f_type_builder_last_seg) = f_type_builder.path.segments.last_mut() {
+            f_type_builder_last_seg.ident = format_ident!("{}Builder", f_type_builder_last_seg.ident);
+        }
+
+        for Attribute { path, tokens, .. } in f_attrs {
+            if let Some(a_ident) = path.get_ident() {
+                if a_ident.eq_str("use_builder") {
+                    if tokens.is_empty() {
+                        is_builder = true;
+                        break
+                    } else {
+                        panic!("The `use_builder` attribute takes no arguments.");
+                    }
+                }
+            }
+        }
 
         let Some(f_type_last_seg) = f_type.path.segments.last() else {
             panic!("Unable to parse type");
         };
 
-        let [new_type, build_ins] = if f_type_last_seg.ident.eq_str("Option") {[
+        let [new_type, build_ins] = if is_builder {[
+            quote! { Option<#f_type_builder> },
+            quote! {
+                match self.#f_ident {
+                    Some(builder) => builder.build(),
+                    None => #f_type::default()
+                }
+            }
+        ]} else if f_type_last_seg.ident.eq_str("Option") {[
             quote! { #f_type },
             quote! { self.#f_ident }
         ]} else {[
@@ -72,7 +100,6 @@ pub fn derive(input: TokenStream) -> TokenStream {
         impl #ident_builder {
             pub fn build(self) -> #ident {
                 #ident {
-                    // TODO: implement different build instructions for nested builder structs
                     #(#field_names: #build_instructions),*
                 }
             }
